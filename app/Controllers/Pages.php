2b394235be3625e6340e371b5be0bddfbfedee60
CestinaLover <axledamaso@gmail.com>
2026-09-12 23:24:49 +0800

    Update POS system

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -6,11 +6,11 @@ class Pages extends BaseController
 {
     public function index()
     {
-        return view('Home');
+        return view('home');
     }
 
     public function about()
     {
-        return view('About');
+        return view('about');
     }
 }
